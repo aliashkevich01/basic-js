4a969ed7e4ca3b2c37ca0d04f2d1ba411112e490
andrei aliashkevich <aliashkevich01@gmail.com>
2021-09-26 19:51:56 +0300

    feat: working on carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,7 +17,11 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-export default function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function dateSample(sampleActivity) {
+  if(typeof sampleActivity!='string'||N<=0||isNaN(sampleActivity)||sampleActivity>MODERN_ACTIVITY){
+    return false;
+  }
+  else{
+    return Math.ceil(Math.log ( ( MODERN_ACTIVITY/N ) / (0.693/HALF_LIFE_PERIOD) ));
+  }
 }
